26ed082350338bb0bc0595a41af6b65d1b473385
unknown <iamjayprashanthc@gmail.com>
2022-09-27 23:23:20 +0530

    Added Geographical Distance

--- Search_Algorithms.py
+++ Search_Algorithms.py
@@ -200,14 +200,14 @@ def astar_search(graph, start, goal):
         return path, explored_nodes
 
     path.append(start)
-    path_cost = get_manhattan_heuristic(start, goal)
+    path_cost = geographical_heuristic(start, goal)
     # Priority Queue to keep sorted distance travelled till now
     frontier = [(path_cost, path)]
     while len(frontier) > 0:
         # pop a node from the queue
         path_cost_till_now, path_till_now = pop_frontier(frontier)
         current_node = path_till_now[-1]
-        path_cost_till_now = path_cost_till_now - get_manhattan_heuristic(current_node, goal)
+        path_cost_till_now = path_cost_till_now - geographical_heuristic(current_node, goal)
         explored_nodes.append(current_node)
         # test goal condition
         if current_node == goal:
@@ -225,7 +225,7 @@ def astar_search(graph, start, goal):
 
             # extra_cost = graph.get_edge_weight(current_node, neighbour)
             extra_cost = 1
-            neighbour_cost = extra_cost + path_cost_till_now + get_manhattan_heuristic(neighbour, goal)
+            neighbour_cost = extra_cost + path_cost_till_now + geographical_heuristic(neighbour, goal)
             new_element = (neighbour_cost, path_to_neighbour)
 
             is_there, indexx, neighbour_old_cost, _ = get_frontier_params_new(neighbour, frontier)
@@ -284,14 +284,14 @@ def get_frontier_params(node, frontier):
     return False, None, None, None
 
 
-def get_manhattan_heuristic(node, goal):
-    i, j = divmod(int(node), 8)
-    i_goal, j_goal = divmod(int(goal), 8)
-    i_delta = abs(i - i_goal)
-    j_delta = abs(j - j_goal)
+#def get_manhattan_heuristic(node, goal):
+#    i, j = divmod(int(node), 8)
+#    i_goal, j_goal = divmod(int(goal), 8)
+#    i_delta = abs(i - i_goal)
+#    j_delta = abs(j - j_goal)
 
-    manhattan_dist = i_delta + j_delta
-    return manhattan_dist
+#    manhattan_dist = i_delta + j_delta
+#    return manhattan_dist
 
 def geographical_heuristic(node,goal):
     i,j =divmod(int(node),4)
@@ -306,22 +306,22 @@ if __name__ == '__main__':
     graph_neighbours = generate_graph()
 
     print("============ UCS Search ================")
-    path_ucs, explored_ucs = uniform_cost_search(graph_neighbours, '0', '3')
+    path_ucs, explored_ucs = uniform_cost_search(graph_neighbours, '0', '49')
     print("Path UCS:", path_ucs)
     # print("Explored Nodes UCS: ", explored_ucs)
     print(len(explored_ucs))
     print()
 
     print("============ AStar Search ================")
-    path_astar, explored_astar = astar_search(graph_neighbours, '0', '3')
+    path_astar, explored_astar = astar_search(graph_neighbours, '0', '49')
     print("Path_astar:", path_astar)
     print("Explored Nodes A Star: ", explored_astar)
     print(len(explored_astar))
     print()
 
     print("============ Bottleneck Astar Search ================")
-    path_1, explored_1 = astar_search(graph_neighbours, '0', '3')
-    path_2, explored_2 = astar_search(graph_neighbours, '0', '3')
+    path_1, explored_1 = astar_search(graph_neighbours, '0', '49')
+    path_2, explored_2 = astar_search(graph_neighbours, '0', '49')
     print("Path1:", path_1)
     print("Path_2:", path_2)
 
